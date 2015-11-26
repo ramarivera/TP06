d69f4c89b3c9ebc62e3b25fabc34b13fc60577e8
martinnarrua <martin94.profugo@gmail.com>
2015-11-26 19:41:38 -0300

    update y delete

--- EJ02/CRUDPersonaFacade.cs
+++ EJ02/CRUDPersonaFacade.cs
@@ -29,12 +29,33 @@ namespace EJ02
 
         public void Update (Persona pPersona)
         {
-           
+            using (this.iUnitOfWork = new UnitOfWork())
+            {
+                this.iUnitOfWork.PersonaRepository.Update(pPersona);
+
+                foreach (Telefono tel in pPersona.Telefonos)
+                {
+                    this.iUnitOfWork.TelefonoRepository.Update(tel);
+                }
+
+                this.iUnitOfWork.Save();
+            }
         }
 
         public void Delete (Persona pPersona)
         {
+            using (this.iUnitOfWork = new UnitOfWork())
+            {
 
+                foreach (Telefono tel in pPersona.Telefonos)
+                {
+                    this.iUnitOfWork.TelefonoRepository.Delete(tel);
+                }
+
+                this.iUnitOfWork.PersonaRepository.Delete(pPersona);
+
+                this.iUnitOfWork.Save();
+            }
         }
 
         public List<Persona> GetAll()
