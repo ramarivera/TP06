6d33f53ee6353f4560d42a03e83c62ddffc56511
RamaRivera <ramarivera@gmail.com>
2015-12-10 19:39:14 -0300

    Cambio de nombre del formulario principal

--- EJ02-GUI/Program.cs
+++ EJ02-GUI/Program.cs
@@ -16,7 +16,7 @@ namespace EJ02_GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new VentanaPrincipal());
         }
     }
 }
